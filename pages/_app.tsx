084794af642f56a84a6f30bda9aa9e51b727fe07
Debayo Tomisin <debayotomisin07@gmail.com>
2022-12-08 17:40:55 +0100

    chore/added toaster to app file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import "../styles/globals.css";
 import { AuthProvider } from "src/services/store";
 import initializeServer from "src/services/apiAdapter";
+import { Toaster } from "react-hot-toast";
 
 initializeServer();
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
+      <Toaster />
       <Component {...pageProps} />
     </AuthProvider>
   );
