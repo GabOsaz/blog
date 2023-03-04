391b5ac352893808a9e0f8590558b04987726ab7
Debayo Tomisin <debayotomisin07@gmail.com>
2023-03-04 14:07:02 +0100

    chore/move use router from view to logout service

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,16 @@
 import Head from "next/head";
-import { useRouter } from "next/router";
 import styles from "../styles/Home.module.css";
 import { userStorageAdapter } from "src/services/storageAdapter";
 import { useLogOutAuthenticate } from "src/application/authenticate";
 
 export default function Home() {
   const storage = userStorageAdapter();
-  const router = useRouter();
   const userName = storage.user.userName;
 
   const { user, logOutAuthenticate } = useLogOutAuthenticate();
 
   const handleLogout = () => {
     logOutAuthenticate();
-    router.push("/login");
   };
 
   return (
