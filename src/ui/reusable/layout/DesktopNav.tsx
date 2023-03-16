5deefb6502c09cd8642919dba8e76b07a1b6c7c5
Debayo Tomisin <debayotomisin07@gmail.com>
2023-03-16 22:33:25 +0100

    styles/nav item styling

--- src/ui/reusable/layout/DesktopNav.tsx
+++ src/ui/reusable/layout/DesktopNav.tsx
@@ -3,11 +3,15 @@ import { useLogOutAuthenticate } from "@/application/authenticate";
 import { userStorageAdapter } from "@/services/storageAdapter";
 import styles from "./layout.module.css";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import path from "path";
 
 function DesktopNav({ children }) {
   const { logOutAuthenticate } = useLogOutAuthenticate();
   const storage = userStorageAdapter();
   const user = storage.user;
+  const { pathname } = useRouter();
+  console.log(pathname);
 
   const handleLogout = () => {
     logOutAuthenticate();
@@ -20,25 +24,57 @@ function DesktopNav({ children }) {
           <span className={styles.logo}>IceBlog</span>
 
           <div className={styles.navContain}>
-            <Link href="/">Home</Link>
-            <Link href="/">Explore</Link>
-            {user && <Link href="/">My Articles</Link>}
+            <Link
+              className={pathname === "/" ? styles.active : styles.navItem}
+              href="/"
+            >
+              Home
+            </Link>
+            <Link
+              className={
+                pathname === "/explore" ? styles.active : styles.navItem
+              }
+              href="/"
+            >
+              Explore
+            </Link>
+            {user && (
+              <Link
+                className={
+                  pathname === "/articles" ? styles.active : styles.navItem
+                }
+                href="/"
+              >
+                My Articles
+              </Link>
+            )}
           </div>
 
           <div className={styles.profileContain}>
             {user ? (
               <div>
-                <span>Profile</span>
+                <Link
+                  className={
+                    pathname === "/profile" ? styles.active : styles.navItem
+                  }
+                  href="/profile"
+                >
+                  Profile
+                </Link>
                 <span onClick={handleLogout}>Logout</span>
               </div>
             ) : (
               <div>
                 <span>
-                  <a href="login">SignIn</a>
+                  <Link className={styles.navItem} href="login">
+                    SignIn
+                  </Link>
                 </span>
                 |
                 <span>
-                  <a href="register"></a>
+                  <Link className={styles.navItem} href="register">
+                    Register
+                  </Link>
                 </span>
               </div>
             )}
